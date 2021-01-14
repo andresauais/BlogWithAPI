aefe0e652a1482091c4d8c5e223a47fc11660522
Andres Auais <andresauais@gmail.com>
2021-01-14 12:01:14 +0000

    main posts

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const link = 'https://jsonplaceholder.typicode.com/posts/';
+
+$.ajax(link, {
+    method: 'GET'
+}).then(
+    function success(data, statusText, jqXHR){
+        console.log(data);
+        createPost(data);
+    },
+    function fail(jqXHR, errorStatusText, errorMessage){
+        console.log(errorMessage);
+    }
+)
+
+function createPost(data){
+    for(index in data){
+        let div = $('<div></div>')
+        div.addClass("postContainer");
+        $('#container').append(div);
+        console.log(div);
+        div.append(createTitle(data[index].title));
+        div.append(createBody(data[index].body));
+    }
+
+    function createTitle(postTitle){
+        let p = $('<p></p>');
+        p.addClass("postTitle");
+        p.text(postTitle);
+        return p;
+    }
+
+    function createBody(postBody){
+        let p = $('<p></p>');
+        p.addClass("postBody");
+        p.text(postBody);
+        return p;
+    }
+}
