b22306cd71b89510dda2f22bea41365fcbf8dfda
Anntroy <ana.troyan@gmail.com>
2021-01-15 12:33:06 +0100

    Styling posts

--- js/main.js
+++ js/main.js
@@ -5,16 +5,81 @@ const link = 'https://jsonplaceholder.typicode.com/posts/',
 const createPost = data => {
     const createTitle = title => $('<h3></h3>').addClass('postTitle').text(title);
     const createBody = body => $('<p></p>').addClass('postBody').text(body);
+    const createUserTitle = user => $('<h4></h4>').addClass('articleTitle').text(user);
+    const createCommentTitle = user => $('<h4></h4>').addClass('articleTitle').text(user);
 
     data.forEach((post, index) => {
         const $article = $('<article></article>');
         const $title = createTitle(post.title);
+        const $hr = $('<hr></hr>');
+        const $divUser = $('<div></div>');
+        const $calendarButton = $('<button></button>');
+        const $calendarIcon = $('<i></i>');
+        const $calendarTitle = $('<h4></h4>');
+        const $userButton = $('<button></button>');
+        const $userIcon = $('<i></i>');
+        const $divEdit = $('<div></div>');
+        const $editButton = $('<button></button>');
+        const $editIcon = $('<i></i>');
+        const $editTitle = $('<h4></h4>');
+        const $deleteButton = $('<button></button>');
+        const $deleteIcon = $('<i></i>');
+        const $deleteTitle = $('<h4></h4>');
+        const $commentButton = $('<button></button>');
+        const $commentIcon = $('<i></i>');
+
         const $body = createBody(post.body);
+        const $userTitle = createUserTitle(post.userId);
+        const $commentTitle = createCommentTitle(post.userId);
+
+        $hr.addClass('hrPost');
+        $divUser.addClass('divUser');
+        $divEdit.addClass('divEdit');
+
+        $calendarButton.addClass('btn').addClass('postButton');
+        $calendarIcon.addClass('fa').addClass('fa-calendar').addClass('fa-lg');
+        $calendarTitle.addClass('articleTitle').text('4:05 PM');
+
+        $userIcon.addClass('fa').addClass('fa-user').addClass('fa-lg');
+        $userButton.addClass('btn').addClass('postButton');
+
+        $editButton.addClass('btn').addClass('postButton');
+        $editIcon.addClass('fa').addClass('fa-edit').addClass('fa-lg');
+        $editTitle.addClass('articleTitle').text('Edit');
+
+        $deleteButton.addClass('btn').addClass('postButton');
+        $deleteIcon.addClass('fa').addClass('fa-trash').addClass('fa-lg');
+        $deleteTitle.addClass('articleTitle').text('Delete');
+
+        $commentButton.addClass('btn').addClass('postButton');
+        $commentIcon.addClass('fa').addClass('fa-comment').addClass('fa-lg');
 
         $article.attr('id', index);
         $article.addClass('postContainer');
         $article.append($title);
+
+        $calendarButton.append($calendarIcon);
+        $calendarButton.append($calendarTitle);
+        $userButton.append($userIcon);
+        $userButton.append($userTitle);
+        $divUser.append($calendarButton);
+        $divUser.append($userButton);
+        $($article).append($divUser);
+        $($article).append($hr);
+
         $article.append($body);
+
+        $editButton.append($editIcon);
+        $editButton.append($editTitle);
+        $deleteButton.append($deleteIcon);
+        $deleteButton.append($deleteTitle);
+        $commentButton.append($commentIcon);
+        $commentButton.append($commentTitle);
+        $divEdit.append($editButton);
+        $divEdit.append($deleteButton);
+        $divEdit.append($commentButton);
+        $($article).append($divEdit);
+
         $('#container').append($article);
     });
 
